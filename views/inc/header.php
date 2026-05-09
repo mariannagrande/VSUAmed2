d125883ae5dd4f5ee2667ec9c98669f976cfe746
no_name <otso8davebelleza@gmail.com>
2026-05-10 01:17:14 +0800

    commit

--- views/inc/header.php
+++ views/inc/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Kaiadmin - Bootstrap 5 Admin Dashboard</title>
+    <title>VSUA -- <?= !empty($_SERVER['PHP_SELF']) ? strtoupper(pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME)) : 'SYSTEM' ?></title>
     <meta
       content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
       name="viewport"
