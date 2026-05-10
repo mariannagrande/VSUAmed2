186a362f5d70fe4db4e7e75090eebff3e818e71b
no_name <otso8davebelleza@gmail.com>
2026-05-11 01:09:38 +0800

    commit

--- views/manage_students.php
+++ views/manage_students.php
@@ -23,22 +23,151 @@
 
         <div class="container">
           <div class="page-inner">
-            
-
 
 
             <!----------------------------Edit Here------------------------------------>
               
             <?php
-              if ($_GET['page'] == "SearchStudent"):
+              if ($_GET['page'] == "SearchStudent"): ?>
 
-                echo "hello bitch add";
+                <div class="row m-5">
+                  <div class="col-md-6">
+                    <div class="card">
 
-              else:
+                    jjjj
+                    </div>
+                  </div>
+                </div>
 
-                echo "hello teh";
-              endif;
-            ?>
+              <?php else: ?>
+
+                <div class="row m-5">
+                  <div class="col-md-6">
+                    <div class="card">
+
+                      <form>
+                        <div class="card-header">
+                          <div class="card-title">ADD STUDENT</div>
+                        </div>
+                      
+                        <div class="card-body">
+
+                          <div class="row">
+
+                            <div class="col-md-8">
+                              <div class="form-group">
+                                <label for="sn">Student Number: </label>
+                                <input
+                                  id="sn"
+                                  class="form-control"
+                                  placeholder="2024-01"
+                                  required
+                                />
+                              </div>
+                            </div>
+
+                            <div class="col-md-4">
+                              <div class="form-group">
+                                <label for="lname">Sex: </label>
+                                <input
+                                  id="lname"
+                                  class="form-control"
+                                  placeholder="Male"
+                                  required
+                                />
+                              </div>
+                            </div>
+
+                          </div>
+
+                          <div class="row">
+
+                            <div class="col-md-6">
+                              <div class="form-group">
+                                <label for="lname">Student Last Name: </label>
+                                <input
+                                  id="lname"
+                                  class="form-control"
+                                  placeholder="Cruz"
+                                  required
+                                />
+                              </div>
+                            </div>
+
+                            <div class="col-md-6">
+                              <div class="form-group">
+                                <label for="lname">Student First Name: </label>
+                                <input
+                                  id="lname"
+                                  class="form-control"
+                                  placeholder="John Doe"
+                                  required
+                                />
+                              </div>
+                            </div>
+
+                          </div>
+
+                          <div class="row">
+
+                            <div class="col-md-4">
+                              <div class="form-group">
+                                <label for="lname">Student Middle Name: </label>
+                                <input
+                                  id="lname"
+                                  class="form-control"
+                                  placeholder="Mendoza"
+                                />
+                              </div>
+                            </div>
+
+                            
+                            <div class="col-md-3">
+                              <div class="form-group">
+                                <label for="lname">Birthday: </label>
+                                <input
+                                  id="lname"
+                                  class="form-control"
+                                  type="date"
+                                  required
+                                />
+                              </div>
+                            </div>
+                            
+                            <div class="col-md-5">
+                              <div class="form-group">
+                                <label for="lname">Email: </label>
+                                <input
+                                  id="lname"
+                                  class="form-control"
+                                  placeholder="johndoe@gmail.com"
+                                  required
+                                />
+                              </div>
+                            </div>
+
+                          </div>
+                          <div class="form-group">
+                            <label for="lname">Contact Number: </label>
+                            <input
+                              id="lname"
+                              class="form-control"
+                              placeholder="Philippines [+ 639]"
+                              required
+                            />
+                          </div>
+                        </div>
+
+                        <div class="card-footer p-3">
+                          <input type="submit" class="btn btn-success ">
+                        </div>
+                      </form>
+
+                    </div>
+                  </div>
+                </div>
+
+            <?php endif; ?>
 
             <!--------------------------End Edit Here---------------------------------->
       
