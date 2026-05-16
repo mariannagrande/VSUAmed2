1c2f2b6f6a6a29861ee77cc804535235045d8235
JoyahMaryJoy <abegaill@example.com>
2026-05-16 23:16:44 +0800

    hello

--- views/inc/footer.php
+++ views/inc/footer.php
@@ -7,24 +7,6 @@
                   ThemeKita
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"> Help </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"> Licenses </a>
-              </li>
-            </ul>
-          </nav>
-          <div class="copyright">
-            2024, made with <i class="fa fa-heart heart text-danger"></i> by
-            <a href="http://www.themekita.com">ThemeKita</a>
-          </div>
-          <div>
-            Distributed by
-            <a target="_blank" href="https://themewagon.com/">ThemeWagon</a>.
-          </div>
-        </div>
-      </footer>
 
 
       <!-- Custom template | don't include it in your project! -->
