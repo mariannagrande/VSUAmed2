33cfb0a7c121fdfe73f5cfbd5b21ee312823d85c
abegailFaith <abegaill@example.com>
2026-05-27 15:19:48 +0800

    commit

--- views/history.php
+++ views/history.php
@@ -106,24 +106,6 @@
                     </div>
                   <?php endforeach; endif; ?>
                 </tbody>
-                <div class="modal fade" id="hello<?= $h['visit_id'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel"><?= strtoupper($h['last_name'] . ", " . $h['first_name'] . " " . $h['middle_name']) ?></h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body">
-                        ...
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
               </table>
             </div>
             
